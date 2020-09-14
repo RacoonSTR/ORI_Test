61a9b59b40de4d019c2656741e7c5246fcfd6f89
RacoonStripped <racoonstr@gmail.com>
2020-09-14 22:50:39 +0300

    Add convertation switch

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
+import importlib
 
 import internal
 import internal_csv
-import pascalvoc
+import pascal_voc
 
 # Test variables
 FOLDER_1_1 = './data/internal'
@@ -13,15 +14,28 @@ FOLDER_2_3 = './result/internalCSV_2'
 FOLDER_3_1 = './data/pascalVOC'
 FOLDER_3_2 = './result/pascalVOC'
 
+INTERNAL = 'internal'
+INTERNAL_CSV = 'internal_csv'
+PASCAL_VOC = 'pascal_voc'
+
 #SOURCE_FOLDER = os.environ('SOURCE_FOLDER')
 #DEST_FOLDER = os.environ('DEST_FOLDER')
 #SOURCE_FORMAT = os.environ('SOURCE_FORMAT')
 #DEST_FORMAT = os.environ('DEST_FORMAT')
 
+SOURCE_FORMAT = INTERNAL
+DEST_FORMAT = INTERNAL_CSV
+
 def main():
-    internal.to_internal_csv(FOLDER_1_1, FOLDER_2_2)
-    internal_csv.to_pascal_voc(FOLDER_2_1, FOLDER_3_2)
-    pascalvoc.to_internal_csv(FOLDER_3_1, FOLDER_2_3)
+    sourse_module = importlib.import_module(SOURCE_FORMAT)
+    if sourse_module is None:
+        print("ERROR: Can't find module for source format")
+    else:
+        convert_method = getattr(sourse_module, 'to_' + DEST_FORMAT, None)
+        if convert_method is None:
+            print("ERROR: Can't find method for destination format")
+        else:
+            convert_method(FOLDER_1_1, FOLDER_2_2)
 
 if __name__ == "__main__":
     main()
